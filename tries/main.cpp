a97564d81216ac71846029d66c51f5af006da1a4
sarthak choudhary <f20190112@pilani.bits-pilani.ac.in>
2020-07-20 03:20:01 +0530

    testing tries

--- tries/main.cpp
+++ tries/main.cpp
@@ -5,4 +5,14 @@ using namespace std;
 
 int main() {
     // code to use trie
+    Trie t;
+    t.insertWord("and");
+    t.insertWord("are");
+    t.insertWord("dot");
+
+    cout << t.searchWord("and") << endl;
+
+    t.removeWord("and");
+    cout << t.searchWord("and") << endl;
+
 }
